bf5518057e5181f06cb5274f85c989ce2d5ec4c5
yilmazderin <dyilmaz2@uwo.ca>
2023-11-03 19:34:22 -0400

    Updated comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Import dependencies
 import streamlit as st
 import spacy
 from textblob import TextBlob
@@ -5,14 +6,7 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 
-def sumy_summarizer(docx):
-    parser = PlaintextParser.from_string(docx, Tokenizer("english"))
-    lex_summarizer = LexRankSummarizer()
-    summary = lex_summarizer(parser.document, 3)
-    summary_list = [str(sentence) for sentence in summary]
-    result = ' '.join(summary_list)
-    return result
-
+#Tokenization function
 def text_analyzer(my_text):
     nlp = spacy.load('en_core_web_sm')
     docx = nlp(my_text)
@@ -20,6 +14,7 @@ def text_analyzer(my_text):
     allData = [('"Tokens":{},\n"Lemma":{}'.format(token.text, token.lemma_)) for token in docx]
     return allData
 
+#Entity extraction function
 def entity(my_text):
     nlp = spacy.load('en_core_web_sm')
     docx = nlp(my_text)
@@ -28,17 +23,30 @@ def entity(my_text):
     allData = ['"Tokens:":{}\n"Entities":{}'.format(tokens, entities)]
     return allData
 
+#Summarization function
+def sumy_summarizer(docx):
+    parser = PlaintextParser.from_string(docx, Tokenizer("english"))
+    lex_summarizer = LexRankSummarizer()
+    summary = lex_summarizer(parser.document, 3)
+    summary_list = [str(sentence) for sentence in summary]
+    result = ' '.join(summary_list)
+    return result
+
+#Main function
 def main():
+    #Title
     st.title("Named Entity Recognition App")
     st.subheader("Try out several natural language processing features!")
 
 
+    #Tokenizing text using spacy
     st.write("First enter your text, then click the button of whichever feature you want to try out!")
     message = st.text_area("Enter your text")
     if st.button("Tokenize your text"):
         nlp_result = text_analyzer(message)
         st.json(nlp_result)
     
+    #Extracting entities from text with spacy
     if st.button("Extract Entities from your text."):
         nlp_result = entity(message)
         st.json(nlp_result)
@@ -56,5 +64,6 @@ def main():
         summary_result = sumy_summarizer(message)
         st.success(summary_result)
 
+#Execute if primary module
 if __name__ == "__main__":
     main()
